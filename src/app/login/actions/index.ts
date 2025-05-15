d4af2a7b209800055cd139d6740daf9178983fe6
biaag10 <107511894+biaag10@users.noreply.github.com>
2025-05-15 01:17:39 -0300

    ajuste nas do back local para o do vercel

--- src/app/login/actions/index.ts
+++ src/app/login/actions/index.ts
@@ -2,7 +2,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_VERCEL_URL;
 
 export const loginUser = async (emailOrUsername: string, password: string) => {
   try {
-    const response = await fetch(`${API_URL}/users/login`, {
+    const response = await fetch('https://express-backend-mongodb.vercel.app/users/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
